83f9744b879683c0158d6de679069cb68b82624f
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2023-11-27 21:42:00 +0700

    add authorization

--- middleware/authorization.go
+++ middleware/authorization.go
@@ -12,13 +12,56 @@ import (
 func ProductAuthorization() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		db := database.GetDB()
-		bookUUID := ctx.Param("bookUUID")
+		productUUID := ctx.Param("productUUID")
 
 		userData := ctx.MustGet("adminData").(jwt.MapClaims)
 		userID := uint(userData["id"].(float64))
 
 		var getProduct models.Product
-		err := db.Select("user_id").Where("uuid = ?", bookUUID).First(&getProduct).Error
+		err := db.Where("uuid = ?", productUUID).First(&getProduct).Error
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   err.Error(),
+				"message": "Data Not Found",
+			})
+			return
+		}
+
+		if getProduct.AdminID != userID {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": "You are not allowed to access this data",
+			})
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
+func VariantAuthorization() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		db := database.GetDB()
+		var getProduct models.Product
+		var getVariant models.Variant
+
+		productUUID := ctx.Param("productUUID")
+
+		userData := ctx.MustGet("adminData").(jwt.MapClaims)
+		userID := uint(userData["id"].(float64)) // adminID
+
+		// get variant productID from UUID
+		err := db.Where("uuid = ?", productUUID).First(&getVariant).Error
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   err.Error(),
+				"message": "Data Not Found",
+			})
+			return
+		}
+
+		// get adminID from productID
+		err = db.Where("id = ?", getVariant.ProductID).First(&getProduct).Error
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
 				"error":   err.Error(),
